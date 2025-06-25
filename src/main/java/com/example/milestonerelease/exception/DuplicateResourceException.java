f45cb62e39f1c7f1cbfed9d972bd3938ae7ba80f
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-25 21:33:09 +0530

    Add Milestone and Release entity classes for milestone and release management

---
+++ src/main/java/com/example/milestonerelease/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.example.milestonerelease.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
