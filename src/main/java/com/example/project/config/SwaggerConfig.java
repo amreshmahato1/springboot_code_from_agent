58d1f3aa4c537825b2ecdeb1ddc835d63493ee01
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-25 16:54:58 +0530

    Add Spring Boot milestone and release management codebase

--- src/main/java/com/example/project/config/SwaggerConfig.java
+++ src/main/java/com/example/project/config/SwaggerConfig.java
@@ -1,18 +1,32 @@
 package com.example.project.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI springMilestoneOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Milestone and Release Association API")
-                        .version("1.0.0")
-                        .description("API documentation for Milestone and Release Association Service"));
+                .info(new Info().title("Milestone & Release API")
+                        .description("API documentation for Milestone Creation and Release Association")
+                        .version("v1.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Wiki")
+                        .url("https://example.com/docs"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("milestone-release")
+                .pathsToMatch("/api/**")
+                .build();
     }
 }
