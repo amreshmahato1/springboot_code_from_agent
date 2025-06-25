58b69a7f782a714ebd6ff56602e2a69be4d7ef4c
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-25 21:26:48 +0530

    Add Spring Boot entity and repository classes for Milestone and Release management

---
+++ config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.example.project.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI milestoneOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Milestone API")
+                        .description("API for Milestone Creation and Release Association")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation")
+                        .url("https://example.com/docs"));
+    }
+
+    @Bean
+    public GroupedOpenApi milestoneApi() {
+        return GroupedOpenApi.builder()
+                .group("milestone")
+                .pathsToMatch("/api/milestones/**")
+                .build();
+    }
+}
