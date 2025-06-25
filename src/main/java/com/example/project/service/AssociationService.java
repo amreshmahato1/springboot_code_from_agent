19080397f40a87d69a540e1839e765a16889f72b
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-25 14:44:19 +0530

    Add Milestone and Release entities for milestone and release association feature

--- src/main/java/com/example/project/service/AssociationService.java
+++ src/main/java/com/example/project/service/AssociationService.java
@@ -1,11 +1,39 @@
 package com.example.project.service;
 
-import com.example.project.entity.Release;
+import com.example.project.dto.ReleaseAssociationResponseDTO;
 import com.example.project.entity.Milestone;
-import com.example.project.exception.ResourceNotFoundException;
+import com.example.project.entity.Release;
+import com.example.project.exception.ReleaseAlreadyAssociatedException;
+import com.example.project.repository.ReleaseRepository;
+import com.example.project.service.MilestoneService;
+import com.example.project.service.ReleaseService;
+import com.example.project.utility.ValidationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface AssociationService {
-    void associateReleaseToMilestone(String releaseId, String milestoneId) throws ResourceNotFoundException;
-    void removeReleaseFromMilestone(String releaseId, String milestoneId) throws ResourceNotFoundException;
-    Milestone getMilestoneForRelease(String releaseId) throws ResourceNotFoundException;
+@Service
+public class AssociationService {
+    private final ReleaseRepository releaseRepository;
+    private final MilestoneService milestoneService;
+    private final ReleaseService releaseService;
+    private final ValidationUtil validationUtil;
+
+    @Autowired
+    public AssociationService(ReleaseRepository releaseRepository, MilestoneService milestoneService, ReleaseService releaseService, ValidationUtil validationUtil) {
+        this.releaseRepository = releaseRepository;
+        this.milestoneService = milestoneService;
+        this.releaseService = releaseService;
+        this.validationUtil = validationUtil;
+    }
+
+    @Transactional
+    public ReleaseAssociationResponseDTO associateReleaseWithMilestone(Long releaseId, Long milestoneId) {
+        Release release = releaseService.findReleaseOrThrow(releaseId);
+        Milestone milestone = milestoneService.findMilestoneOrThrow(milestoneId);
+        validationUtil.validateReleaseNotAlreadyAssociated(release);
+        release.setMilestoneId(milestone.getId());
+        releaseRepository.save(release);
+        return new ReleaseAssociationResponseDTO(release.getId(), milestone.getId(), "Release associated with milestone successfully.");
+    }
 }
