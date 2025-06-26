30bf8c1fb6764060a215341bfd5a05dc938d2799
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-26 12:47:58 +0530

    Add Milestone Creation and Release Association Spring Boot module

--- src/main/java/com/example/project/exception/InvalidDateRangeException.java
+++ src/main/java/com/example/project/exception/InvalidDateRangeException.java
@@ -4,4 +4,8 @@ public class InvalidDateRangeException extends RuntimeException {
     public InvalidDateRangeException(String message) {
         super(message);
     }
+
+    public InvalidDateRangeException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
