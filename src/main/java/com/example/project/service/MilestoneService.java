f1d3c207ca9b75d7f443d50d287c041c5a0a37b8
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-26 12:47:55 +0530

    Add Milestone Creation and Release Association Spring Boot module

--- src/main/java/com/example/project/service/MilestoneService.java
+++ src/main/java/com/example/project/service/MilestoneService.java
@@ -1,8 +1,35 @@
 package com.example.project.service;
 
-import com.example.project.dto.MilestoneRequestDto;
-import com.example.project.dto.MilestoneResponseDto;
+import com.example.project.entity.Milestone;
+import com.example.project.repository.MilestoneRepository;
+import com.example.project.exception.ValidationException;
+import com.example.project.util.ValidationUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public interface MilestoneService {
-    MilestoneResponseDto createMilestone(MilestoneRequestDto requestDto);
+@Service
+public class MilestoneService {
+    private final MilestoneRepository milestoneRepository;
+    private final ValidationUtil validationUtil;
+
+    @Autowired
+    public MilestoneService(MilestoneRepository milestoneRepository, ValidationUtil validationUtil) {
+        this.milestoneRepository = milestoneRepository;
+        this.validationUtil = validationUtil;
+    }
+
+    public Milestone createMilestone(Milestone milestone) {
+        validateMilestone(milestone);
+        return milestoneRepository.save(milestone);
+    }
+
+    public void validateMilestone(Milestone milestone) {
+        if (!validationUtil.isValidMilestoneName(milestone.getName())) {
+            throw new ValidationException("Invalid milestone name");
+        }
+        if (!validationUtil.isValidMilestoneDates(milestone.getStartDate(), milestone.getEndDate())) {
+            throw new ValidationException("Milestone end date must be after start date");
+        }
+        // Add more validation as per LLD
+    }
 }
