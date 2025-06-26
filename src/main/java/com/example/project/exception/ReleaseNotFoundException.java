036d3c791ce91013921138bc24a16a75f742e7c9
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-26 12:47:57 +0530

    Add Milestone Creation and Release Association Spring Boot module

--- src/main/java/com/example/project/exception/ReleaseNotFoundException.java
+++ src/main/java/com/example/project/exception/ReleaseNotFoundException.java
@@ -4,4 +4,8 @@ public class ReleaseNotFoundException extends RuntimeException {
     public ReleaseNotFoundException(String message) {
         super(message);
     }
+
+    public ReleaseNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
