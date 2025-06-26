5d2ac5dbbfd73a78e4dd717e3d743e950d24cbc5
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-26 12:47:56 +0530

    Add Milestone Creation and Release Association Spring Boot module

--- src/main/java/com/example/project/exception/MilestoneNotFoundException.java
+++ src/main/java/com/example/project/exception/MilestoneNotFoundException.java
@@ -4,4 +4,8 @@ public class MilestoneNotFoundException extends RuntimeException {
     public MilestoneNotFoundException(String message) {
         super(message);
     }
+
+    public MilestoneNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
