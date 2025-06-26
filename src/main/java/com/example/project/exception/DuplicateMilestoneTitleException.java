b66ec0b74ed656996c0feb24e153b3c71f9a68f2
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-26 13:28:15 +0530

    Add Spring Boot milestone and release association codebase

--- src/main/java/com/example/project/exception/DuplicateMilestoneTitleException.java
+++ src/main/java/com/example/project/exception/DuplicateMilestoneTitleException.java
@@ -4,8 +4,4 @@ public class DuplicateMilestoneTitleException extends RuntimeException {
     public DuplicateMilestoneTitleException(String message) {
         super(message);
     }
-
-    public DuplicateMilestoneTitleException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
