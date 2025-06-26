c6d9e4eac8b6f45eeb427be18df60e70d909c23b
amreshmahato1 <amresh.mahato@ascendion.com>
2025-06-26 13:28:17 +0530

    Add Spring Boot milestone and release association codebase

--- src/main/java/com/example/project/exception/ReleaseNotFoundException.java
+++ src/main/java/com/example/project/exception/ReleaseNotFoundException.java
@@ -4,8 +4,4 @@ public class ReleaseNotFoundException extends RuntimeException {
     public ReleaseNotFoundException(String message) {
         super(message);
     }
-
-    public ReleaseNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
